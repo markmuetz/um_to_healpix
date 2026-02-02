3d69e31b3f0feab9b4d198e8532757492a3362ee
Mark Muetzelfeldt <markmuetz@gmail.com>
2026-02-02 15:32:26 +0000

    Functions.

--- output_testing/iris_plot_precip.py
+++ output_testing/iris_plot_precip.py
@@ -1,44 +1,67 @@
-# coding: utf-8
 import iris
 import matplotlib.pyplot as plt
 import numpy as np
+
 import um_to_healpix.util
-import matplotlib.pyplot as plt
 
 
-sim = 'glm.n2560_RAL3p3.tuned'
-config = um_to_healpix.util.load_config('../config/hk26_config.py')
-sim_config = config.processing_config[sin]
+def plot_zonal_mean(pr, zonal_mean, lat):
+    time_coord = pr.coord('time')
+    times = time_coord.units.num2date(time_coord.points)
+    t1 = times[0]
+    t2 = times[-1]
 
-cubes = iris.load(sorted((sim_config['basedir'] / 'field.pp/apverb.pp').glob('*.pp'))[0])
-for i, cube in enumerate(cubes):
-    print(i, repr(cube))
+    plt.figure(layout='constrained')
+    plt.title(f'{t1} - {t2}')
 
-rain = cubes[24]
-snow = cubes[28]
-pr = rain.copy()
-pr.data += snow.data
+    plt.plot(lat, zonal_mean)
 
-lat = pr.coord('latitude').points
-
-pr_time_mean = pr.data.mean(axis=0)
-weights = np.cos(np.deg2rad(lat))
-weights_2d = weights[:, np.newaxis] * np.ones(pr.data.shape[2])[np.newaxis, :]
-zonal_mean = np.sum(pr_time_mean * weights_2d, axis=1) / np.sum(weights_2d, axis=1)
+    plt.xlim((-90, 90))
+    plt.xlabel('lat')
+    plt.ylabel(f'precipitation_flux ({pr.units})')
 
 
-time_coord = cube.coord('time')
-times = time_coord.units.num2date(time_coord.points)
-t1 = times[0]
-t2 = times[-1]
+def calc_zonal_mean(pr, lat):
+    pr_time_mean = pr.data.mean(axis=0)
+    weights = np.cos(np.deg2rad(lat))
+    weights_2d = weights[:, np.newaxis] * np.ones(pr.data.shape[2])[np.newaxis, :]
+    zonal_mean = np.sum(pr_time_mean * weights_2d, axis=1) / np.sum(weights_2d, axis=1)
+    return zonal_mean
 
-plt.figure(layout='constrained')
-plt.title(f'{t1} - {t2}')
 
-plt.plot(lat, zonal_mean)
+def load_data(sim_config):
+    cubes = iris.load(sorted((sim_config['basedir'] / 'field.pp/apverb.pp').glob('*.pp'))[0])
 
-plt.xlim((-90, 90))
-plt.xlabel('lat')
-plt.ylabel(f'precipitation_flux ({pr.units})')
+    rain = cubes.extract_cube(
+        iris.Constraint(name='stratiform_rainfall_flux') &
+        iris.Constraint(cube_func=um_to_healpix.util.cube_cell_method_is_not_empty)
+    )
+    snow = cubes.extract_cube(
+        iris.Constraint(name='stratiform_snowfall_flux') &
+        iris.Constraint(cube_func=um_to_healpix.util.cube_cell_method_is_not_empty)
+    )
+    print(repr(rain))
+    print(repr(snow))
 
-plt.savefig('/home/users/mmuetz/tmp/pr_zonal_mean.png')
+    pr = rain.copy()
+    pr.data += snow.data
+    return dict(rain=rain, snow=snow, pr=pr)
+
+
+def main(sim, config):
+    sim_config = config.processing_config[sim]
+
+    precips = load_data(sim_config)
+    for name, cube in precips.items():
+        print(name)
+        lat = cube.coord('latitude').points
+        zonal_mean = calc_zonal_mean(cube, lat)
+        plot_zonal_mean(cube, zonal_mean, lat)
+        plt.savefig(f'/home/users/mmuetz/tmp/{name}_zonal_mean.png')
+
+
+if __name__ == '__main__':
+    sim = 'glm.n2560_RAL3p3.tuned'
+    config = um_to_healpix.util.load_config('../config/hk26_config.py')
+    main(sim, config)
+
